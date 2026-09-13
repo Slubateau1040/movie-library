697701e40e3f65ba296d82ff223ac4d7b8c9f342
Slubateau1040 <elouan.taupinn@proton.me>
2026-09-13 14:08:40 +0200

    Finish CRUD for Movie

--- movie/urls.py
+++ movie/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.listMovies, name='listMovies'),
-    path('<int:movieId>', views.detailMovie, name='detailMovie'),
+    path('<int:movie_id>', views.detailMovie, name='detailMovie'),
+    path('addMovie/', views.addMovie, name='addMovie'),
+    path('<int:movie_id>/delete/', views.deleteMovie, name='deleteMovie'),
+    path('<int:movie_id>/edit/', views.editMovie, name='editMovie'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,5 @@
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
@@ -10,3 +12,38 @@ def listMovies(request):
 def detailMovie(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
     return render(request, 'movie/detailMovie.html', {'movie': movie})
+
+def addMovie(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        genre = request.POST['genre']
+        platform = request.POST['platform']
+        is_finished = request.POST.get('is_finished', False) == 'on'
+        rating = request.POST['rating']
+        Movie.objects.create(title=title, genre=genre, platform=platform, is_finished=is_finished, rating=rating)
+        return HttpResponseRedirect(reverse('listMovies'))
+    else :
+        return render(request, 'movie/addMovie.html')
+
+def deleteMovie(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    movie.delete()
+    return HttpResponseRedirect(reverse('listMovies'))
+
+def editMovie(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    if request.method == 'POST':
+        title = request.POST['title']
+        genre = request.POST['genre']
+        platform = request.POST['platform']
+        is_finished = request.POST.get('is_finished', False) == 'on'
+        rating = request.POST['rating']
+        movie.title = title
+        movie.genre = genre
+        movie.platform = platform
+        movie.is_finished = is_finished
+        movie.rating = rating
+        movie.save()
+        return HttpResponseRedirect(reverse('listMovies'))
+    else :
+        return render(request, 'movie/editMovie.html', {'movie': movie})
